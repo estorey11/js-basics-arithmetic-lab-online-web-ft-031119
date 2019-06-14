22ca93abeb47c41d97d774cdb35346ef11589331
Eric Storey <estorey@gmail.com>
2019-06-14 18:51:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here
-var1=31
-var2=2
-multiply=var1*var2;
-random=Math.floor(Math.random() * 10) + 1;
-mod=14%5;
-max=Math.max(10, 20);
+const var1=31
+const var2=2
+const multiply=var1*var2;
+const random=Math.floor(Math.random() * 10) + 1;
+const mod=14%5;
+const max=Math.max(10, 20);
